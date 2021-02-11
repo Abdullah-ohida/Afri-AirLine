851f388baa7c301fca294e028ec89c9577e0683d
Abdullah Ismail <ohida2001@gmail.com>
2021-02-11 22:45:06 +0100

    project setup updated

---
+++ test/com/passengers/PassengerTest.java
@@ -0,0 +1,17 @@
+package com.passengers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassengerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
